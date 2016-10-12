06a2f7b3f28ebd7ca2485ef8ff06c92c1c566d10
Samuel Johnston <Samueljohnston96@gmail.com>
2016-10-12 13:36:39 -0400

    adding main with globals

--- main.cpp
+++ main.cpp
@@ -5,6 +5,29 @@
 #include "parser.h"
 #include "simulator.h"
 
+
+
+/*
+		Define n as the number of processes to simulate. Note that this is determined via the input
+		file described below.
+		
+		Define m as the number of processors (i.e., cores) available within the CPU (use a default
+		value of 1). Note that we could use this in a future project.
+		
+		Define t_cs as the time, in milliseconds, that it takes to perform a context switch (use a
+		default value of 8). Remember that a context switch occurs each time a process leaves the
+		CPU and is replaced by another process.
+		
+		For the RR algorithm, define the aforementioned time slice (i.e., t_slice) value, measured
+		in milliseconds, with a default value of 84.
+*/
+int n = -1;
+int m = 1;
+int t_slice = 84;
+int t_cs = 8;
+
+
+
 int main(int argc, char const *argv[])
 {
 	if (argc < 2) {
@@ -12,10 +35,12 @@ int main(int argc, char const *argv[])
 		return 1;
 	}
 	// PrintFile(argv[1]);
-	auto vp = Parse(argv[1]); //TODO ask, is this allowed?
+	std::vector< class Process*> vp = Parse(argv[1]); //TODO ask, is this allowed?
 
 	Simulator sim(vp);
 	sim.pprint();
+	sim.run();
+
 
 }
 
